0ddeb4f9214bc97ae347fc435c82252ecb2be772
Harrison <harrison.vieira@yduqs.com.br>
2023-05-04 23:25:09 -0300

    criação de paginas conecção de socket

--- backend-prototype/src/battle/battle.gateway.ts
+++ backend-prototype/src/battle/battle.gateway.ts
@@ -10,13 +10,14 @@ import { Server, Socket } from 'socket.io';
 import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt.guard';
 
-@WebSocketGateway({ namespace: 'battle' })
+@WebSocketGateway({ namespace: 'battle', cors: '*:*' })
 export class BattleGateway {
   @WebSocketServer() server: Server;
   constructor(private readonly battleService: BattleService) {}
-  @UseGuards(JwtAuthGuard)
-  @SubscribeMessage('battleMatch')
-  battleMatch(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
+
+  //@UseGuards(JwtAuthGuard)
+  @SubscribeMessage('findMatch')
+  findMatch(@MessageBody() data: string, @ConnectedSocket() client: Socket) {
     console.log(data);
     return this.battleService.battleMatch();
   }
