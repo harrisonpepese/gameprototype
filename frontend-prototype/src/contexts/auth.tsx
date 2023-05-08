0da5fb6ec4b3854656370d06de78fb1412155e84
Leonardo Tandeli <leonardotandelicw@outlook.com>
2023-05-08 14:44:51 -0300

    fix auth

--- frontend-prototype/src/contexts/auth.tsx
+++ frontend-prototype/src/contexts/auth.tsx
@@ -18,21 +18,46 @@ const defaultContext: AuthContextType = {
   logout: () => null,
   token: undefined,
 };
-
+const isBrowser = () => typeof window !== "undefined";
 export const AuthContext = createContext<AuthContextType>(defaultContext);
 
 export function AuthProvider({ children }: PropsWithChildren<any>) {
-  const [token, setToken] = useState<string>();
+  let tokenLocalStorage: string | null = "";
+  if (typeof window !== "undefined") {
+    tokenLocalStorage = localStorage.getItem("session");
+  }
+  const [token, setToken] = useState(
+    tokenLocalStorage ? tokenLocalStorage : defaultContext.token
+  );
+
   const router = useRouter();
+  const unprotectedRoutes = ["/login", "/signup"];
+  const pathIsProtected = !unprotectedRoutes.includes(router.pathname);
+
+  useEffect(() => {
+    localStorage.setItem("session", JSON.stringify(token));
+  }, [token]);
+
+  useEffect(() => {
+    if (!token && pathIsProtected) {
+      router.push("/login");
+    }
+    if (token && !pathIsProtected) {
+      router.push("/");
+    }
+  }, [token, router, pathIsProtected]);
+
   const login = (username: string, password: string) => {
     GameApi.post("/auth", { username, password }).then((res) => {
       const { access_token } = res.data;
+      localStorage.setItem("session", access_token);
       setToken(access_token);
       router.push("/");
     });
   };
   const logout = () => {
     setToken(undefined);
+    localStorage.removeItem("session");
     router.push("/login");
   };
   return (
